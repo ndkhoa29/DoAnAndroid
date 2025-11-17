73411d9084ddfffc6fe2d89889acefd632fdcfa8
ndkhoa29 <dangkhoadk29@gmail.com>
2025-11-17 10:47:42 +0700

    create booking screen

---
+++ app/src/main/java/com/example/homeserviceapp/UpcomingAdapter.java
@@ -0,0 +1,70 @@
+package com.example.homeserviceapp;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.List;
+
+public class UpcomingAdapter extends RecyclerView.Adapter<UpcomingAdapter.BookingViewHolder> {
+
+    private List<BookingItem> bookingList;
+
+    public UpcomingAdapter(List<BookingItem> bookingList) {
+        this.bookingList = bookingList;
+    }
+
+    @NonNull
+    @Override
+    public BookingViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_booking_upcoming, parent, false);
+        return new BookingViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull BookingViewHolder holder, int position) {
+        BookingItem booking = bookingList.get(position);
+
+        holder.tvBookingId.setText(booking.getBookingId());
+        holder.tvBookingDate.setText(booking.getDate());
+        holder.tvServiceName.setText(booking.getServiceName());
+        holder.tvLocation.setText(booking.getLocation());
+        holder.tvTime.setText(booking.getTime());
+        holder.tvPrice.setText(booking.getPrice());
+        holder.ivServiceImage.setImageResource(booking.getImageResId());
+
+        holder.btnCancelBooking.setOnClickListener(v -> {
+            Toast.makeText(v.getContext(),
+                    "Đã huỷ đơn: " + booking.getBookingId(),
+                    Toast.LENGTH_SHORT).show();
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return bookingList.size();
+    }
+
+    static class BookingViewHolder extends RecyclerView.ViewHolder {
+        TextView tvBookingId, tvBookingDate, tvServiceName;
+        TextView tvLocation, tvTime, tvPrice, btnCancelBooking;
+        ImageView ivServiceImage;
+
+        BookingViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvBookingId = itemView.findViewById(R.id.tvBookingId);
+            tvBookingDate = itemView.findViewById(R.id.tvBookingDate);
+            tvServiceName = itemView.findViewById(R.id.tvServiceName);
+            tvLocation = itemView.findViewById(R.id.tvLocation);
+            tvTime = itemView.findViewById(R.id.tvTime);
+            tvPrice = itemView.findViewById(R.id.tvPrice);
+            ivServiceImage = itemView.findViewById(R.id.ivServiceImage);
+            btnCancelBooking = itemView.findViewById(R.id.btnCancel);
+        }
+    }
+}
