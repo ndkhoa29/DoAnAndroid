8d2230215dea502783914ce464d1cd6cb9dbba13
ndkhoa29 <dangkhoadk29@gmail.com>
2025-11-17 08:53:38 +0700

    create home nav

---
+++ app/src/main/java/com/example/homeserviceapp/BannerItem.java
@@ -0,0 +1,13 @@
+package com.example.homeserviceapp;
+
+public class BannerItem {
+    private int imageResId;
+
+    public BannerItem(int imageResId) {
+        this.imageResId = imageResId;
+    }
+
+    public int getImageResId() {
+        return imageResId;
+    }
+}
